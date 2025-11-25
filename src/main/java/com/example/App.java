f1e04374e8e6a770e36550a0f990553e094dffbd
Dorin Patric Caliman <patric.caliman@endava.com>
2025-11-25 16:40:25 +0200

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,5 @@
+package com.example;
+public class App {
+  public static void main(String[] args) { System.out.println("Hello CI"); }
+  public static int sum(int a, int b) { return a + b; }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,6 @@
+package com.example;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class AppTest {
+  @Test void testSum() { assertEquals(5, App.sum(2,3)); }
+}
